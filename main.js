3e0b68f20b9fbc29e0c48c1be478742aa9f940e9
“alerenna” <alessandrorennafm@gmail.com>
2025-06-06 12:31:36 +0200

    Aggiungi la funzione creaThrottler per limitare l'esecuzione di callback

--- main.js
+++ main.js
@@ -187,6 +187,22 @@ Scrivi una funzione creaThrottler che accetta una funzione e un tempo `limite`.
 Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
 */
 
+function creaThrottler(callback, limite) {
+    let ultimaEsecuzione = 0
+
+    return function (...args) {
+        const ora = Date.now();
+
+        if (ora - ultimaEsecuzione > - limite) {
+            ultimaEsecuzione = ora;
+            callback(...args)
+        } else {
+            console.log("non posso eseguire!");
+
+        }
+    }
+}
+
 
 
 
